64353f9eaaf6073cb960354891d6ada2126f370b
Zdeshniy <zdeshniy@live.com>
2018-08-28 14:29:00 +0500

    Изменение вида метода

--- src/Test.php
+++ src/Test.php
@@ -5,4 +5,18 @@ class Test{
     public function testo(){
         return 'all ok! ';
     }
+    
+    public function automate($str){
+        
+        try{
+        
+            if(!preg_match('/^[\(\)\\s\\n\\t\\r]*$/i', $str))
+                throw new \InvalidArgumentException("Неверная строка!");
+        
+        } catch (\Exception $ex) {
+            return $ex->getMessage();
+        }
+        
+        return true;
+    }
 }
